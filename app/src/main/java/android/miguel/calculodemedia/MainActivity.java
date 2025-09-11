fcced6ce9af4abebb24e80a8d7ebcc94275a0afd
android <Android@uirapuru.br>
2025-09-11 15:21:55 -0300

    Primeiro Commit

---
+++ app/src/main/java/android/miguel/calculodemedia/MainActivity.java
@@ -0,0 +1,20 @@
+package android.miguel.calculodemedia;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+    public class MainActivity extends AppCompatActivity {
+
+        private EditText nota1, nota2, nota3, nota4, numeroFaltas;
+
+        private Button btnCalcular;
+
+        private TextView resultado;
+
+        private String nome;
+    }
